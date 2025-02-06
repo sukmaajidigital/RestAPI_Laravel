8dc929f513eb63b3d6251c7cd037c4610733b53f
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2025-02-06 13:37:02 +0700

    add readme

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
